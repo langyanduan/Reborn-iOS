78952c9033ce22820239944254d68bf1145b563f
langyanduan <langyanduan@gmail.com>
2018-04-28 00:20:46 +0800

    add picker

--- src/screens/Language.js
+++ src/screens/Language.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Text } from "react-native";
+import PickerList from "../components/PickerList";
 
 export default class Language extends React.Component {
   render() {
     return (
-      <Text>Language</Text>
+      <Picker items={['a', 'b', 'c']} selectedIndex={2} onPicker={(index, item) => console.log('' + index + ', ' + item)} />
+      // <Text>Language</Text>
     );
   }
 }
--- src/screens/ServerEditor.js
+++ src/screens/ServerEditor.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { View, ScrollView, Text, Button } from "react-native";
+import { StyleSheet, View, ScrollView, Text, Button } from "react-native";
 import { Actions } from "react-native-router-flux";
 
 import ListItem from "../components/ListItem";
+import ListInputItem from "../components/ListInputItem";
 
 export default class ServerEditor extends React.Component {
   static navigationOptions = ({navigation}) => {
@@ -13,18 +14,51 @@ export default class ServerEditor extends React.Component {
   }
 
   render() {
+    const FormShadowsocks = () => {
+      return [
+        <ListInputItem title="server" />,
+        <ListInputItem title="port" />,
+        <ListItem title="方法" />,
+        <ListInputItem title="password" />,
+      ];
+    }
+
+    const FormSocks = () => {
+      return [
+        <ListInputItem title="server" />,
+        <ListInputItem title="port" />,
+        <ListInputItem title="account" />,
+        <ListInputItem title="password" />,
+      ];
+    }
+    const FormHTTP = FormSocks;
+
+    const Form = () => FormShadowsocks().map((element, index) => React.cloneElement(element, {key: index.toString()}))
+
     return (
-      <ScrollView>
-        <View>
-          <Text>ServerEditor</Text>
+      <ScrollView contentContainerStyle={{paddingVertical: 20}}>
+        <View style={styles.section}>
+          <ListItem 
+            title="Type"
+            subtitle="Shadowsocks"
+            hasArrow
+          />
         </View>
-        <View>
-          <Text>ServerEditor</Text>
-        </View>
-        <View>
-          <Text>ServerEditor</Text>
+        <View style={[styles.section, {marginTop: 30}]}>
+          <Form />
         </View>
       </ScrollView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+
+  },
+  section: {
+    borderTopWidth: StyleSheet.hairlineWidth,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderColor: 'black',
+  },
+});
