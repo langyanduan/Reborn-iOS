ab27a23db1dbc9b90c89c53efa88fba15c909dc5
langyanduan <langyanduan@gmail.com>
2018-05-08 10:47:24 +0800

    add lazy to tabs

--- src/navigators/AppNavigator.js
+++ src/navigators/AppNavigator.js
@@ -15,20 +15,20 @@ import About from "../screens/About";
 import Language  from "../screens/Language";
 
 export default AppNavigator = Actions.create(
-  <Tabs key="tabbar" showLabel={false}>
+  <Tabs key="tabbar" showLabel={false} lazy>
     <Stack key="homeTab" icon={TabBarItem} titleName="Home" iconName="home" initial={true}>
-      <Scene key="home" title="main" component={Home} />
-      <Scene key="serverEditor" title="editor" component={ServerEditor} />
+      <Scene key="home" title="Home" component={Home} />
+      <Scene key="serverEditor" title="Editor" component={ServerEditor} />
     </Stack>
-    <Stack key="configTab" icon={TabBarItem} titleName="Config" iconName="albums">
-      <Scene key="config" title="Config" component={Config} />
-      <Scene key="configDetail" title="ConfigDetail" component={ConfigDetail} />
+    <Stack key="configTab" icon={TabBarItem} titleName="Config" iconName="albums" lazy>
+      <Scene key="config" title="Config Files" component={Config} />
+      <Scene key="configDetail" title="Detail" component={ConfigDetail} />
     </Stack>
-    <Stack key="toolsTab" icon={TabBarItem} titleName="Tools" iconName="build">
-      {/* <Scene key="ping" title="Ping" component={Ping} /> */}
+    <Stack key="toolsTab" icon={TabBarItem} titleName="Tools" iconName="build" lazy>
       <Scene key="tools" title="Tools" component={Tools} />
+      <Scene key="ping" title="Ping" component={Ping} />
     </Stack>
-    <Stack key="settingTab" icon={TabBarItem} titleName="Settings" iconName="settings">
+    <Stack key="settingTab" icon={TabBarItem} titleName="Settings" iconName="settings" lazy>
       <Scene key="setting" title="Settings" component={Settings} />
       <Scene key="thanks" title="Thanks" component={Thanks} />
       <Scene key="language" title="Language" component={Language} />
